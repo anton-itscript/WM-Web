c0a771b0d508a8df1feff7535264cca3f9c49c62
Anton Chukhlomin <hello@itscript.com>
2015-10-26 12:54:52 -0700

    DB connection

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-print "Hello, I'm WM"
+print "Hello, I'm WM";
 
+$conn = new mysqli("db", "root", "gfhjkm", "mysql");
+if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+
+$result = $conn->query("show tables");
+print_r($result);
+
+$conn->close();
 ?>
