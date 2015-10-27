1580f923645dc4a97743928f4b798175ad84ac27
Anton Chukhlomin <hello@itscript.com>
2015-10-27 09:30:26 -0700

    db connection

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
-<?PHP
+<?php
 
-phpinfo();
+$conn = new mysqli("db", "admin", "gfhjkm", "mysql");
+if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
+$result = $conn->query("show tables");
+print_r($result);
+$conn->close();
 
 ?>
