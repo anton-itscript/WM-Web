833d702fd2d9caf9030bc4c4e51402db79be6b36
Anton Chukhlomin <hello@itscript.com>
2015-10-27 10:06:01 -0700

    mysql user

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("db", "admin", "gfhjkm", "mysql");
+$conn = new mysqli("db", "root", "", "mysql");
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
 $result = $conn->query("show tables");
 print_r($result);
