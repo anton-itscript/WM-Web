c4ed6b7f606ef5f21a810122c33731f00b43157b
Dmitriy Gorkovoy <dmitriy.gorkovoy@itscript.com>
2015-11-03 16:34:14 +0600

    host fix

--- application/protected/config/params/application_params.php
+++ application/protected/config/params/application_params.php
@@ -3,7 +3,7 @@
   'php_exe_path' => '/usr/bin/php',
   'mysqldump_exe_path' => NULL,
   'mysql_exe_path' => NULL,
-  'site_url_for_console' => 'http://localhost:9999',
+  'site_url_for_console' => 'http://'.$_SERVER["HTTP_HOST"],
   'console_app_path' => '/var/www/protected/models/../../www/console.php',
   'backups_path' => '/var/www/protected/models/../../www/files/backups',
 ) 
