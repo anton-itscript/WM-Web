c4751786d291847dac8241222c2e69ea05a80282
Dmitriy Gorkovoy <dmitriy.gorkovoy@itscript.com>
2015-11-06 11:49:58 +0600

    fix pathes migration

--- application/protected/migrations/m151105_113200_PathesForDockerVolumes.php
+++ application/protected/migrations/m151105_113200_PathesForDockerVolumes.php
@@ -5,8 +5,8 @@ class m151105_113200_PathesForDockerVolumes extends CDbMigration
 	protected $table = 'settings';
 	public function up()
 	{
-		$this->update($this->table, array('scheduled_reports_path'=>'/usr/share/nginx/html/www/files/weather_monitor_reports'),'setting_id=1');
-		$this->update($this->table, array('xml_messages_path'=>'/usr/share/nginx/html/www/files/xml_messages'),'setting_id=1');
+		$this->update($this->table, array('scheduled_reports_path'=>"/usr/share/nginx/html/www/files/weather_monitor_reports"),'setting_id=1');
+		$this->update($this->table, array('xml_messages_path'=>"/usr/share/nginx/html/www/files/xml_messages"),'setting_id=1');
 	}
 
 	public function down()
